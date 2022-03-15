250bc5d126186729fc3657e6e27d849d8b22439b
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2022-03-15 19:37:57 +0100

    Let's not make this publicly available just yet

--- src/main/java/be/mathiasbosman/alphaessenergyidproxy/controller/ProxyController.java
+++ src/main/java/be/mathiasbosman/alphaessenergyidproxy/controller/ProxyController.java
@@ -3,13 +3,10 @@ package be.mathiasbosman.alphaessenergyidproxy.controller;
 import be.mathiasbosman.alphaessenergyidproxy.domain.DataCollector;
 import be.mathiasbosman.alphaessenergyidproxy.domain.ExportService;
 import be.mathiasbosman.alphaessenergyidproxy.domain.PvStatistics;
-import be.mathiasbosman.alphaessenergyidproxy.domain.energyid.EnergyIdWebhookAdapter;
-import be.mathiasbosman.alphaessenergyidproxy.domain.energyid.dto.MeterReadingsDto;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,17 +20,10 @@ public class ProxyController {
 
   private final DataCollector dataCollector;
   private final ExportService exportService;
-  private final EnergyIdWebhookAdapter webhookAdapter;
 
   @GetMapping("/trigger/weeklyExport")
   public ResponseEntity<Object> triggerWeeklyExport() {
     exportService.exportStatisticsForPastWeek();
-    return ResponseEntity.ok().build();
-  }
-
-  @PostMapping("/export")
-  public ResponseEntity<Object> exportData(MeterReadingsDto dto) {
-    webhookAdapter.postReadings(dto);
     return ResponseEntity.ok().build();
   }
 
